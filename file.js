16014b716ac26814f434fa1d4abc7cc45523413e
VishnuVardhan <vishnuvardhan200510@gmail.com>
2025-08-27 17:32:41 +0530

    added code snippets

---
+++ file.js
@@ -0,0 +1,17 @@
+// Hello World
+console.log("Hello, World!");
+
+// Variables (let, const)
+let name = "Vishnu";
+const age = 20;
+console.log(`My name is ${name}, I am ${age} years old`);
+
+// Function
+function add(a, b) {
+  return a + b;
+}
+console.log(add(5, 7));
+
+// Arrow Function
+const square = (x) => x * x;
+console.log(square(4));
---
+++ loops.py
@@ -0,0 +1,19 @@
+# For loop - simple range
+for i in range(5):   # 0 to 4
+    print("For loop:", i)
+
+# For loop with list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print("Fruit:", fruit)
+
+# While loop
+count = 1
+while count <= 5:
+    print("While loop:", count)
+    count += 1
+
+# Nested loop
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"i={i}, j={j}")
